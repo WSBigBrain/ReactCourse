a80481d60159de6131d2df9d89eb56be52242360
Moana Faber <kahimomo@gmail.com>
2021-12-06 10:50:40 +0100

    Ajout firebase et login

---
+++ le-cosmos/src/Login.js
@@ -0,0 +1,105 @@
+import { Form, Input, Button, Checkbox, message } from 'antd';
+import {
+    getAuth,
+    signInWithEmailAndPassword
+} from "firebase/auth"
+import { initializeApp } from "firebase/app"
+import { firebaseConfig } from "../base";
+import Dashboard from './Dashboard';
+import { useState } from 'react';
+import { LoadingOutlined } from '@ant-design/icons';
+
+const firebaseApp = initializeApp(firebaseConfig);
+const auth = getAuth(firebaseApp);
+
+const Login = (props) => {
+
+  const [loading, setLoading] = useState(false)
+
+  const onFinish = (values) => {
+    setLoading(<LoadingOutlined/>)
+    signInWithEmailAndPassword(auth, values.email, values.password)
+    .then((credentials) => {
+        console.log(credentials);
+        setLoading(false);
+        props.setActiveComponent(<Dashboard/>);
+    })
+    .catch((err) => {
+        console.log(err.message);
+        setLoading(false);
+        message.error(err.message);
+    })
+  };
+
+  const onFinishFailed = (errorInfo) => {
+    console.log('Failed:', errorInfo);
+  };
+
+  return (
+    <Form
+      name="basic"
+      labelCol={{
+        span: 8,
+      }}
+      wrapperCol={{
+        span: 16,
+      }}
+      initialValues={{
+        remember: true,
+      }}
+      onFinish={onFinish}
+      onFinishFailed={onFinishFailed}
+      autoComplete="off"
+    >
+      <Form.Item
+        label="Email"
+        name="email"
+        rules={[
+          {
+            required: true,
+            message: 'Please input your email!',
+          },
+        ]}
+      >
+        <Input />
+      </Form.Item>
+
+      <Form.Item
+        label="Mot de passe"
+        name="password"
+        rules={[
+          {
+            required: true,
+            message: 'Please input your password!',
+          },
+        ]}
+      >
+        <Input.Password />
+      </Form.Item>
+
+      <Form.Item
+        name="remember"
+        valuePropName="checked"
+        wrapperCol={{
+          offset: 8,
+          span: 16,
+        }}
+      >
+        <Checkbox>Remember me</Checkbox>
+      </Form.Item>
+
+      <Form.Item
+        wrapperCol={{
+          offset: 8,
+          span: 16,
+        }}
+      >
+        <Button type="primary" htmlType="submit">
+          Se connecter {loading}
+        </Button>
+      </Form.Item>
+    </Form>
+  );
+};
+
+export default Login;
---
+++ le-cosmos/src/firebaseCredentials.js
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+    apiKey: "AIzaSyA6vTitv1TzyPgdY_Q8oHSfIeKabG_AeLw",
+    authDomain: "react-76c63.firebaseapp.com",
+    projectId: "react-76c63",
+    storageBucket: "react-76c63.appspot.com",
+    messagingSenderId: "118429314574",
+    appId: "1:118429314574:web:100122347fb2de39c4965f",
+};
